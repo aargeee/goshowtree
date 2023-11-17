109151196029d8763bfbaed0a9a5075e3146ad4e
= <=>
2023-11-17 16:52:51 +0530

    Tree Reads one file in our FS

--- goshowtree.go
+++ goshowtree.go
@@ -1,1 +1,26 @@
 package goshowtree
+
+import "io/fs"
+
+type Tree struct {
+	Name    string
+	SubTree []Tree
+}
+
+func BuildTree(fsys fs.FS) Tree {
+	s, _ := fs.Glob(fsys, "*")
+
+	tree := Tree{
+		Name:    ".",
+		SubTree: []Tree{},
+	}
+
+	for _, name := range s {
+		tree.SubTree = append(tree.SubTree, Tree{
+			Name:    name,
+			SubTree: []Tree{},
+		})
+	}
+
+	return tree
+}
