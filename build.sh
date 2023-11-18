0ec9f927fd20d443cba5cd66f6738aaeb2b10c5a
= <=>
2023-11-18 14:27:31 +0530

    added bin/ folder

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ echo "Running go mod tidy..." && go mod tidy && echo "All dependencies installed
 cd cmd;
 echo "Building Project..." && go build -o ../bin/goshowtree && echo "Build Complete";
 echo "";
-echo "You can find gobuildtree in the bin folder";
+echo "You can find gobuildtree in the bin/ folder";
 echo "Type ./gobuildtree <path> to view your tree";
